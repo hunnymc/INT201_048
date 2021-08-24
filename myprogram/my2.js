6257269bc96fe3335d791c5618fd999e91d685b7
hunnymc <69097896+hunnymc@users.noreply.github.com>
2021-08-24 11:59:47 +0700

    Update my2.js

---
+++ myprogram/my2.js
@@ -0,0 +1,35 @@
+//ค้นหาราศีตามปีเกิด
+let input = 1900;
+let year;
+let z;
+
+year = input%12
+
+    if (year === 0) {
+    z = "Monkey";
+    } else if (year === 1) {
+    z = "Rooster";
+    } else if (year === 2) {
+    z = "Dog";
+    } else if (year === 3) {
+    z = "Pig";
+    } else if (year === 4) {
+    z = "Rat";
+    } else if (year === 5) {
+    z = "Ox";
+    } else if (year === 6) {
+    z = "Tiger";
+    } else if (year === 7) {
+    z = "Rabbit";
+    } else if (year === 8) {
+    z = "Dragon";
+    } else if (year === 9) {
+    z = "Snake";
+    } else if (year === 10) {
+    z = "Horse";
+    } else if  (year === 11) {
+    z = "Sheep";
+    }
+
+ console.log(z)
+
