a308aead8680ba1a0d30fb95411b40175d8f4a9a
hunnymc <69097896+hunnymc@users.noreply.github.com>
2021-08-31 14:14:18 +0700

    Create my3.js

---
+++ myprogram/my3.js
@@ -0,0 +1,22 @@
+//Simple Objact
+let people = {
+    name: 'sinon',
+    age: 21,
+    gender: 'male',
+}
+
+//Object Value is array
+let profile = {
+    id: 001,
+    hobby: [ 'game', 'music'],
+}
+
+//Aggregated object
+let game = {
+    name: 'GoD  oF gEnIeR',
+    production: {
+        Dicecter : 'sekiro',
+        acter : 'hekii?YU',
+        music : 'LAble Team'
+    }
+}
